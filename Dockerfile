a50a13a50710daad15c92d4eb7d3fd7a0cea0136
SoloSynth1 <orixay@hotmail.com>
2020-12-21 13:49:09 +0800

    add install-airflow.sh; delete unused Dockerfile

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM alpine
-
-COPY . /app
-
-WORKDIR /app
---
+++ install-airflow.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+source ./venv/bin/activate
+
+pip install apache-airflow==1.10.13 \
+ --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-1.10.13/constraints-3.8.txt" \
+ airflow initdb
