583e49b3f2f9aeb2053d5cb02db2ebe6661e3b8e
Orix Au Yeung <orixay@hotmail.com>
2020-12-22 13:27:33 +0800

    Update bash.py

--- dags/bash.py
+++ dags/bash.py
@@ -10,9 +10,9 @@ from airflow.utils.dates import days_ago
 # These args will get passed on to each operator
 # You can override them on a per-task basis during operator initialization
 default_args = {
-    'owner': 'airflow',
+    'owner': 'orix.auyeung',
     'depends_on_past': False,
-    'email': ['airflow@example.com'],
+    'email': ['orix.auyeung@hkmci.com'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
@@ -32,7 +32,7 @@ default_args = {
     # 'trigger_rule': 'all_success'
 }
 dag = DAG(
-    'bash_test',
+    'bash_test2',
     default_args=default_args,
     description='A simple test DAG',
     schedule_interval=timedelta(days=1),
