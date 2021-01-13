05b54c197dcea02cf14b57c24d92fc1ab01b35ee
SoloSynth1 <orixay@hotmail.com>
2021-01-13 22:48:38 +0800

    add dummy task

--- dags/apps_analytics.py
+++ dags/apps_analytics.py
@@ -123,9 +123,11 @@ playstore_analytics_reporter = KubernetesPodOperator(namespace='airflow',
 start = DummyOperator(task_id="start", dag=dag)
 end = DummyOperator(task_id="end", dag=dag)
 appstore_analytics = DummyOperator(task_id="appstore-analytics", dag=dag)
+playstore_analytics = DummyOperator(task_id="playstore-analytics", dag=dag)
 
-start >> run_id_generator >> [appstore_analytics, playstore_analytics_worker]
+start >> run_id_generator >> [appstore_analytics, playstore_analytics]
 appstore_analytics >> appstore_analytics_workers
+playstore_analytics >> playstore_analytics_worker
 appstore_analytics_workers >> appstore_analytics_reporter
 playstore_analytics_worker >> playstore_analytics_reporter
 [appstore_analytics_reporter, playstore_analytics_reporter] >> end
