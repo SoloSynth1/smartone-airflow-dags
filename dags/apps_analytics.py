6699e0b688106f2fae27b8f7d7d66d935ccf40b1
SoloSynth1 <orixay@hotmail.com>
2021-01-28 16:17:04 +0800

    refactor to create common.py; cleanup

--- dags/apps_analytics.py
+++ dags/apps_analytics.py
@@ -6,7 +6,8 @@ from airflow.operators.dummy_operator import DummyOperator
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
-# from kubernetes.client import models as k8s
+
+from .common import common_pod_args, crawler_pod_args
 
 
 def generate_run_id():
@@ -38,58 +39,31 @@ default_args = {
     # 'trigger_rule': 'all_success'
 }
 
-# crawler_affinity = k8s.V1Affinity(
-#     node_affinity=k8s.V1NodeAffinity(
-#         required_during_scheduling_ignored_during_execution=[
-#             k8s.V1NodeSelector(
-#                 node_selector_terms=[
-#                     k8s.V1NodeSelectorTerm(
-#                         match_expressions=[
-#                             k8s.V1NodeSelectorRequirement(key='node-type', operator='In', values=['crawler-node'])
-#                         ],
-#                     )
-#                 ]
-#             )
-#         ]
-#     ),
-#     pod_affinity=k8s.V1PodAntiAffinity(
-#         required_during_scheduling_ignored_during_execution=[
-#             k8s.V1PodAffinityTerm(
-#                 label_selector=k8s.V1LabelSelector(
-#                     match_expressions=[
-#                         k8s.V1LabelSelectorRequirement(key="pod-type", operator="In", values="crawler-pod")
-#                     ]
-#                 ),
-#                 topology_key="failure-domain.beta.kubernetes.io/zone",
-#             )
-#         ]
-#     ),
-# )
-crawler_affinity = {
-    'nodeAffinity': {
-        'requiredDuringSchedulingIgnoredDuringExecution': {
-            'nodeSelectorTerms': [{
-                'matchExpressions': [{
-                    'key': 'cloud.google.com/gke-nodepool',
-                    'operator': 'In',
-                    'values': ['crawler-pool']
-                }]
-            }]
-        }
-    },
-    'podAntiAffinity': {
-        'requiredDuringSchedulingIgnoredDuringExecution': [{
-            'labelSelector': {
-                'matchExpressions': [{
-                    'key': 'pod-type',
-                    'operator': 'In',
-                    'values': ['crawler-pod']
-                }]
-            },
-            'topologyKey': 'kubernetes.io/hostname'
-        }]
-    },
-}
+# crawler_affinity = {
+#     'nodeAffinity': {
+#         'requiredDuringSchedulingIgnoredDuringExecution': {
+#             'nodeSelectorTerms': [{
+#                 'matchExpressions': [{
+#                     'key': 'cloud.google.com/gke-nodepool',
+#                     'operator': 'In',
+#                     'values': ['crawler-pool']
+#                 }]
+#             }]
+#         }
+#     },
+#     'podAntiAffinity': {
+#         'requiredDuringSchedulingIgnoredDuringExecution': [{
+#             'labelSelector': {
+#                 'matchExpressions': [{
+#                     'key': 'pod-type',
+#                     'operator': 'In',
+#                     'values': ['crawler-pod']
+#                 }]
+#             },
+#             'topologyKey': 'kubernetes.io/hostname'
+#         }]
+#     },
+# }
 
 dag = DAG(
     'apps_analytics',
@@ -111,26 +85,23 @@ appstore_analytics_workers = [KubernetesPodOperator(namespace='airflow',
                                                     image="gcr.io/smartone-gcp-1/appstore-scrapy:latest",
                                                     name="appstore-analytics-worker-{}".format(idx),
                                                     task_id="appstore-analytics-worker-{}".format(idx),
-                                                    get_logs=True,
                                                     dag=dag,
                                                     env_vars={
                                                         "RUN_ID": "{{ ti.xcom_pull(task_ids='run-id-generator') }}",
                                                         "APPSTORE_RANGE_REGEX": regex_range,
                                                     },
                                                     resources={
-                                                        "request_memory": "1024Mi",
+                                                        "request_memory": "500Mi",
                                                         "request_cpu": "300m",
                                                     },
-                                                    affinity=crawler_affinity,
-                                                    labels={'pod-type': 'crawler-pod'},
-                                                    image_pull_policy='Always',
-                                                    is_delete_operator_pod=True, ) for idx, regex_range in enumerate(appstore_analytics_worker_regex_range)]
+                                                    **common_pod_args,
+                                                    **crawler_pod_args,
+                                                    ) for idx, regex_range in enumerate(appstore_analytics_worker_regex_range)]
 
 appstore_analytics_reporter = KubernetesPodOperator(namespace='airflow',
                                                     image="gcr.io/smartone-gcp-1/reporter:latest",
                                                     name="appstore-analytics-reporter",
                                                     task_id="appstore-analytics-reporter",
-                                                    get_logs=True,
                                                     dag=dag,
                                                     env_vars={
                                                         "RUN_ID": "{{ ti.xcom_pull(task_ids='run-id-generator') }}",
@@ -142,31 +113,29 @@ appstore_analytics_reporter = KubernetesPodOperator(namespace='airflow',
                                                         "request_cpu": "1200m",
                                                         "request_ephemeral_storage": "20Gi",
                                                     },
-                                                    image_pull_policy='Always',
-                                                    is_delete_operator_pod=True, )
+                                                    **common_pod_args,
+                                                    )
 
 playstore_analytics_worker = KubernetesPodOperator(namespace='airflow',
                                                    image="gcr.io/smartone-gcp-1/playstore-analytics-worker:latest",
                                                    name="playstore-analytics-worker",
                                                    task_id="playstore-analytics-worker",
-                                                   get_logs=True,
                                                    dag=dag,
                                                    env_vars={
                                                        "RUN_ID": "{{ ti.xcom_pull(task_ids='run-id-generator') }}",
                                                    },
                                                    resources={
-                                                       "request_memory": "1024Mi",
+                                                       "request_memory": "500Mi",
                                                        "request_cpu": "300m",
                                                    },
-                                                   affinity=crawler_affinity,
-                                                   labels={'pod-type': 'crawler-pod'},
-                                                   image_pull_policy='Always',
-                                                   is_delete_operator_pod=True, )
+                                                   **common_pod_args,
+                                                   **crawler_pod_args,
+                                                   )
+
 playstore_analytics_reporter = KubernetesPodOperator(namespace='airflow',
                                                      image="gcr.io/smartone-gcp-1/reporter:latest",
                                                      name="playstore-analytics-reporter",
                                                      task_id="playstore-analytics-reporter",
-                                                     get_logs=True,
                                                      dag=dag,
                                                      env_vars={
                                                          "RUN_ID": "{{ ti.xcom_pull(task_ids='run-id-generator') }}",
@@ -178,8 +147,8 @@ playstore_analytics_reporter = KubernetesPodOperator(namespace='airflow',
                                                          "request_cpu": "1200m",
                                                          "request_ephemeral_storage": "20Gi",
                                                      },
-                                                     image_pull_policy='Always',
-                                                     is_delete_operator_pod=True, )
+                                                     **common_pod_args,
+                                                     )
 
 start = DummyOperator(task_id="start", dag=dag)
 end = DummyOperator(task_id="end", dag=dag)
